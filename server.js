6c479bda60fe44dbeef93bc2e75c6c7fcfd786db
unknown <kunjparekh310@gmail.com>
2023-01-22 23:56:05 +0530

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express'); // Include ExpressJS 
-const app = express(); // Create an ExpressJS app
-const BodyParser = require('body-parser');
-const port = 3000
+const express = require('express'); // Include ExpressJS
+const app = express()// Create an ExpressJS app
+const bodyParser = require('body-parser'); // Middleware
 
-app.use(BodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Route Homepage
+// Route to Homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Nav/index.html');
-  });
+  res.sendFile(__dirname + '/templates/index.html');
+});
 
-// Route to Loginpage
+// Route to Login Page
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/Nav/login.html');
-  });
- 
-app.post('/login', (req, res) => {
-    // Insert Login Code Here
-    let username = req.body.username;
-    let password = req.body.password;
-    res.send(`Username: ${username} Password: ${password}`);
-  });
+  res.sendFile(__dirname + '/templates/login.html');
+});
 
+app.post('/login', (req, res) => {
+  // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
+});
+
+const port = 3000 // Port we will listen on
+
+// Function to listen on the port
 app.listen(port, () => console.log(`This app is listening on port ${port}`));
